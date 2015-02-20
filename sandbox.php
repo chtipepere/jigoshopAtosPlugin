072b35d159c7e6c1f489c3597aabf1967d3891da
πR <chtipepere@gmail.com>
2015-02-20 23:36:33 +0100

    Remove unused file

--- sandbox.php
+++
@@ -1,7 +0,0 @@
-<?php
-include( '../../../wp-load.php' );
-
-$post_data['DATA'] = 'sandbox'; //Needed
-$post_data['sandbox'] = 'NULL!0!2!005009461440410!fr!100!8755900!CB!10-02-2012!11:50!10-02-2012!004!certif!22!978!4974!545!1!22!Comp!sandbox!return!caddie!Merci!fr!fr!001!741!my@email.com!1.10.21.192!30!direct!data';
-
-$response = wp_remote_post( 'automatic_response.php', [ 'body' => $post_data ] );
